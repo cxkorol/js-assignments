fbc5e4030189f46a0aa334eb6662ce30c136a900
korol <cx.korol@gmail.com>
2019-04-14 17:02:32 +0300

    feat: solve array task

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -38,7 +38,7 @@ function findElement(arr, value) {
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
 function generateOdds(len) {
-   throw new Error('Not implemented');
+   return Array.from({length: len}, (x, i) => i * 2 +1);
 }
 
 /**
